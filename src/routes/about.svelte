c6f5655fcfe840d3b2065ffa5cc5d8a196d8f534
Lena Schnedlitz <git.lena.schnedlitz@web.de>
2021-04-09 20:16:19 +0200

    Move photo to 'about' page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -2,6 +2,32 @@
   <title>Lena Schnedlitz - About</title>
 </svelte:head>
 
+<script>
+  let src = '../../me/optimized/me-s-min.jpg';
+</script>
+
+<style>
+  img {
+    width: 100%;
+    box-shadow: 0 1.25rem 1.25rem -25px rgba(0, 0, 0, .3);
+  }
+
+  @media (min-width: 700px) {
+    img {
+      width: 80%;
+      margin-left: 10%;
+    }
+  }
+
+  @media (min-width: 800px) {
+    img {
+      box-shadow: none;
+      margin-top: 2rem;
+      margin-bottom: -2.5rem;
+    }
+  }
+</style>
+
 <article>
   <h1>About Me</h1>
 
@@ -19,4 +45,6 @@
     content related to programming, but I also post about topics like math puzzles,
     origami, and video games.
   </p>
+
+  <img {src} alt="That's me"/>
 </article>
