72c15128c6216a9a00000e23e5a8fb84bcbe53bc
Lena Schnedlitz <git.lena.schnedlitz@web.de>
2021-11-14 22:01:00 +0100

    Hide header on scroll down

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -3,17 +3,34 @@
   import Logo from '../components/Logo.svelte';
   import Aside from '../components/Aside.svelte';
   import DarkModeToggle from '../components/DarkModeToggle.svelte';
+  import {onMount} from 'svelte';
 
   export let segment;
+
+  let oldPosition = 0;
+  let visible = true;
+
+  onMount(() => {
+    window.onscroll = function () {
+      visible = window.scrollY < 64 || window.scrollY < oldPosition;
+      oldPosition = window.scrollY;
+    };
+  });
 </script>
 
 <style>
   header {
+    position: fixed;
+    width: 100vw;
     display: flex;
     justify-content: flex-end;
     padding: 1rem;
 
     --item-padding: .75rem;
+  }
+
+  header:not(.visible) {
+    display: none;
   }
 
   @media all and (min-width: 768px) {
@@ -27,7 +44,7 @@
   }
 </style>
 
-<header>
+<header class:visible>
   <Logo/>
   <Nav {segment}/>
   <DarkModeToggle/>
